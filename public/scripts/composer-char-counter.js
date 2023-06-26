ce9576eae679d8a4e67b4a3397706c2787edc413
John <john.cloghesy@gmail.com>
2023-06-26 10:36:11 -0600

    ADD CHAR COUNTER - Add Listener/Handler: Upon char input show available
     - Establish event listener for text input, focused on text area  (.new-tweet section of Tweet form)
     - Obtain the text area values, counts inputted chars & compares to max to determine Available 
     - Update the counter to show char still available upon text entry
    
     M04WK08-611
     [11.3:  Implement the character counter] - reference to specific webpage in Compass

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,11 +4,30 @@
 
 
 /**
- *  
- * Document Ready Function - ensures HTML elements are loaded prior to accessing
- *                         - ensures browser loads these HTML elements
+ * Implements character counter function (listener, handler, still available) 
+ *   DOCUMENT READY - starts by ensuring HTML elements are loaded in browser
+ *        - loading occurs prior to any attempts to access elements  
+ *   EVENT LISTENER - Establishes event listener for text input  
+ *        - listener focused on textarea  (.new-tweet section of Tweet form)
+ *        - obtains the textarea values 
+ *   ANALYSIS - counts inputted chars & compares to max to determine available 
+ *	 RESPONDS - updates the displayed counter showing char still available 
+ *          - determines styling of counter by applying class based upon inputted
+ *            values vs. max allowed (turns red if inputted too mnny characters)
  */
 $(document).ready(function () {
+  $('.new-tweet textarea').on('input', function() {
+    let newTweetLength = $(this).val().length;
+    let nearbyCounter = $(this).siblings('.counter');
+    const tweetLengthLimit = 140;
+
+    if (newTweetLength > tweetLengthLimit) {
+      nearbyCounter.addClass('tweetTooLong');
+    } else if (newTweetLength <= tweetLengthLimit) {
+      nearbyCounter.removeClass('tweetTooLong');
+    }
+    nearbyCounter.text(tweetLengthLimit - newTweetLength);
+  });
 });
 
 
