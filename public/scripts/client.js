cacd21255abe0beb98834a39426e4e2b8e49f4a0
John <john.cloghesy@gmail.com>
2023-06-27 13:03:03 -0600

    DYNAMIC TWEETS: Refactor File: Add title, sections, labels - client.js
     - Organize/structure file - add file title, main sections and any subsections 
    
    M04WK09-623
     [05.1: Dynamic Tweets] - reference to specific webpage in Compass

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,16 @@
+/* ============================================================================================= */
+/* ==--------------------------------   CLIENT.JS      -----------------------------------------== */
+/* ============================================================================================= */
+/* =========================   INSTRUCTIONS   ============================== */
+/*  Initial Instructions                                                     */
+    /** 
+    *  - Client-side JS logic goes here
+    *  - jQuery is already loaded
+    *  - Reminder: Use (and do all your DOM work in) jQuery's document ready function
+    */
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+  *  Event handler - using jQuery to ensure all html element loaded before calling
+  *   - executed when the DOM is fully loaded and ready for manipulation
+  */
  */
 
