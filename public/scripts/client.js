b6a5ea8915dc5e94fd313df78705827dfc0bfb45
John <john.cloghesy@gmail.com>
2023-06-27 13:25:25 -0600

    DYNAMIC TWEETS: Add Data: Add tweet data in an array - tweet data array
     - Add tweet data - array contains all tweet data appropriately structure
     - Use in initial implementation of functions where data is 'hardcoded' 
    
     M04WK09-623
      [05.1: Dynamic Tweets] - reference to specific webpage in Compass

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,5 +16,46 @@
   */
   $(document).ready(function() {
 
- */
+/* =========================   TWEET DATA                     ============== */
+  /** Tweet data - array                                                     */
+    /*  
+    *  tweet data - array contains all tweet data appropriately structure
+    *  using in initial implementation where data is 'hardcoded' 
+    */
+    const data = [
+      {
+        "user": {
+          "name": "Newton",
+          "avatars": "https://i.imgur.com/73hZDYK.png"
+          ,
+          "handle": "@SirIsaac"
+        },
+        "content": {
+          "text": "If I have seen further it is by standing on the shoulders of giants"
+        },
+        "created_at": 1461116232227
+      },
+      {
+        "user": {
+          "name": "Descartes",
+          "avatars": "https://i.imgur.com/nlhLi3I.png",
+          "handle": "@rd" },
+        "content": {
+          "text": "Je pense , donc je suis"
+        },
+        "created_at": 1461113959088
+      },
+      {
+        "user": {
+          "name": "Caitlin",
+          "avatars": "https://i.imgur.com/nlhLi3I.png",
+          "handle": "@me_ing" },
+        "content": {
+          "text": "I am testing an additional object!"
+        },
+        "created_at": 1461113959088
+      }
+    ]
 
+
+  
