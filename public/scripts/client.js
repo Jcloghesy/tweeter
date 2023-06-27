61b4fffe23e6ac507b889d572138799ef4c912ac
John <john.cloghesy@gmail.com>
2023-06-27 13:11:44 -0600

    DYNAMIC TWEETS: Add Function: Add JQuery document event handler - .ready
     - Add event handler - using jQuery to ensure all html element loaded before calling
     - Ensure DOM is fully loaded and ready for manipulation
    
     M04WK09-623
      [05.1: Dynamic Tweets] - reference to specific webpage in Compass

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,9 +8,13 @@
     *  - jQuery is already loaded
     *  - Reminder: Use (and do all your DOM work in) jQuery's document ready function
     */
-/*
+
+/* =========================   EVENT HANDLER - JQUERY READY   ============== */
+  /*
   *  Event handler - using jQuery to ensure all html element loaded before calling
   *   - executed when the DOM is fully loaded and ready for manipulation
   */
+  $(document).ready(function() {
+
  */
 
