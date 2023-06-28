3a5adc29819bdbc35ea821a222142e62ca4dd504
John <john.cloghesy@gmail.com>
2023-06-27 21:31:56 -0600

    DISPLAYING VALIDATION ERRORS: Add HTML, Replace Alert, Hide on Submission
    Add HTML for error messages
             - Create an HTML element that will contain the error messages
             - Locate within the new-tweet section
             - Ensure message is hidden when form is first displayed
    
    Replace Alert Calls: Add hide/show error element
            - Replace the alert calls with jQuery calls that hide/show the error element.
    	- Ensure message is hidden when form is first displayed
    	- Use the slideDown jQuery function for some simple animation
    Hide errors upon submission 
    	- Upon submission and prior to validation, hide the error element
    	- Ensure error message slides back up before you proceed with validation
    	- Ensures users see the error element reappear with a new error message
    
     M04WK09-630
     [08.1: Displaying Validation Errors With jQuery] - reference to specific webpage in Compass

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -31,6 +31,7 @@
       let div = document.createElement('div');
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
+    };
   /**  Calculation - daysSinceTweet function    */
     /**
      * Takes a date in epoch time
@@ -105,53 +106,49 @@
       }
     };
 
-/* ==========   EVENT HANDLERS (Compose-new, submit new)      ============== */
+/** Event Handler - New Tweet Form Submission (remove/slide-up error mess)  */
+    /**
+     * Triggered - Form Submission Triggered on submission of the form with the class 'new-tweet'
+     * Existing Messages - emplie & slides up Empties and slides up any existing error messages
+     * Checks the length of the text being submitted and runs error messages if necessary
+     * POST text -> /tweets/ - If no errors from text length, makes POST request with form text to /tweets/
+     * GET /text/  - then makes a GET request to /tweets/
+     *     Reset , Render to add new 
+     * - Then resets the form and uses our renderTweets function to add the new tweet to the page 
+     */
+    $('.new-tweet form').submit(function(event) {
+      event.preventDefault();
+      $('.new-tweet p').empty().slideUp();
+      const $form = $(this);
+      const newTweetTextStr = $form.children('textarea').val();
 
-  /** Event Listener to Prevent Default Behavior Submit  */
-
-  /** Event Listener to Prevent Default Behavior Submit  
-   *  J/Query POST event handler that serialized form data & submits
-  */
-  $('.new-tweet form').submit(function(event) {
-    event.preventDefault();
-    $('.new-tweet p').empty().slideUp();
-    const $form = $(this);
-    const newTweetTextStr = $form.children('textarea').val();
-
-    if (!newTweetTextStr) {
-      $('.new-tweet p').append('<b>Error:</b> All tweets must contain at least one character. Your tweet currently does not.');
-      setTimeout(() => {
-        $('.new-tweet p').slideDown();
-      }, 600);
-    } else if (newTweetTextStr.length > 140) {
-      $('.new-tweet p').append("<b>Error:</b> We do not accept tweets longer than 140 characters. Your tweet is currently too long.");
-      setTimeout(() => {
-        $('.new-tweet p').slideDown();
-      }, 600);
-    } else {
-      const tweet = $form.serialize();
-       $.ajax({ url: "/tweets/", method: 'POST', data: tweet })
-      .then (function (postRequestReturnValue) {
-        return $.ajax('/tweets', { method: 'GET' })
-      })
-      .then (function (getRequestReturnValue) {
-        const latestTweet = [getRequestReturnValue[getRequestReturnValue.length - 1]];
-        renderTweets(latestTweet);
-      })
-      .then (function(successfulPost) {
-        return $.ajax('/tweets/', { method: 'GET' })
-      })
-      .then (function(allTweetsArr) {
-        $form[0].reset();
-        $form.children('span').text(140);
-        const latestTweet = [allTweetsArr[allTweetsArr.length - 1]];
-        renderTweets(latestTweet);
-      })
-      .fail(function(err) {
-        alert(err.responseJSON.error);
-      })
-    }
-    });
+      if (!newTweetTextStr) {
+        $('.new-tweet p').append('<b>Error:</b> All tweets must contain at least one character. Your tweet currently does not.');
+        setTimeout(() => {
+          $('.new-tweet p').slideDown();
+        }, 600);
+      } else if (newTweetTextStr.length > 140) {
+        $('.new-tweet p').append("<b>Error:</b> We do not accept tweets longer than 140 characters. Your tweet is currently too long.");
+        setTimeout(() => {
+          $('.new-tweet p').slideDown();
+        }, 600);
+      } else {
+        const tweet = $form.serialize();
+        $.ajax({ url: "/tweets/", method: 'POST', data: tweet })
+        .then (function(successfulPost) {
+          return $.ajax('/tweets/', { method: 'GET' })
+        })
+        .then (function(allTweetsArr) {
+          $form[0].reset();
+          $form.children('span').text(140);
+          const latestTweet = [allTweetsArr[allTweetsArr.length - 1]];
+          renderTweets(latestTweet);
+        })
+        .fail(function(err) {
+          alert(err.responseJSON.error);
+        })
+      }
+      });
   
 
   
